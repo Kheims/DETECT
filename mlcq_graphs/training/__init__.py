7faccca622e852b0fad4fe450273a911cb81fc82
Kheims <djamelmesbah141@gmail.com>
2026-03-01 13:04:09 +0100

    extend training __init__.py to export Trainer and TrainResult

--- mlcq_graphs/training/__init__.py
+++ mlcq_graphs/training/__init__.py
@@ -1,8 +1,18 @@
-from .losses import build_loss_fn, compute_class_weights, FocalLoss, WeightedBCELoss
+from __future__ import annotations
+
+from mlcq_graphs.training.losses import (
+    WeightedBCELoss,
+    FocalLoss,
+    build_loss_fn,
+    compute_class_weights,
+)
+from mlcq_graphs.training.trainer import Trainer, TrainResult
 
 __all__ = [
+    "WeightedBCELoss",
+    "FocalLoss",
     "build_loss_fn",
     "compute_class_weights",
-    "FocalLoss",
-    "WeightedBCELoss",
+    "Trainer",
+    "TrainResult",
 ]
